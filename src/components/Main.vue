4f3bc0b680fbb8206337943cf7a343c61fbd133a
2585479524 <2585479524@qq.com>
2019-05-10 07:38:10 +0800

    完善上传

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,44 +1,162 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <p style="font-size:30px; font-weight:600; margin:0 auto;">{{ msg }}</p>
     <Upload
-        multiple
-        type="drag"
-        action="http://l.rebright.top/fileSystem.php">
-        <div style="padding: 20px 0">
-            <Icon type="ios-cloud-upload" size="100" style="color: #3399ff"></Icon>
-            <p>Click or drag files here to upload</p>
-        </div>
+      class="uploadFiled"
+      multiple
+      ref="upload"
+      type="drag"
+      action="http://b.rebright.top/fileSystem.php"
+      :max-size="204800"
+      :on-exceeded-size="handleMaxSize"
+      :format="['doc', 'docx', 'pdf', 'rar', '7z', 'zip']"
+      :on-format-error="handleFormatError"
+      :before-upload="handleUpload"
+      :on-success="uploadSuccess"
+      :on-error="uploadError"
+    >
+      <div style="padding: 20px 0">
+        <Icon type="ios-cloud-upload" size="100" style="color: #004465"></Icon>
+        <p>Click or drag files here to upload</p>
+      </div>
     </Upload>
+    <div
+      class="uploadList"
+      v-if="file.length !== 0"
+      v-for="(item,index) in file"
+    >上传文件: {{ file[index].name }}</div>
+    <div class="information">
+      <Input
+        prefix="ios-finger-print"
+        size="large"
+        placeholder="请输入学号"
+        style="width: auto"
+        v-model="userID"
+      />
+      <br>
+      <Input
+        prefix="ios-contact"
+        size="large"
+        placeholder="请输入姓名"
+        style="width: auto"
+        v-model="username"
+      />
+    </div>
+    <div class="submit">
+      <Button
+        type="success"
+        size="large"
+        @click="upload"
+        :loading="loadingStatus"
+      >{{ loadingStatus ? '上传中' : '提交' }}</Button>
+      <Button type="error" size="large" @click="reset">重置</Button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
+  data() {
+    return {
+      userID: "",
+      username: "",
+      file: [], // 总文件List
+      uploadFile: [], // 需要上传的文件List
+      loadingStatus: false
+    };
+  },
   props: {
     msg: String
+  },
+  methods: {
+    handleUpload(file) {
+      // 随机生成 keyid 值，方便后面删除操作
+      let keyID = Math.random()
+        .toString()
+        .substr(2);
+      file["keyID"] = keyID;
+      this.file.push(file);
+      this.uploadFile.push(file);
+      // 阻止默认事件
+      return false;
+    },
+    handleFormatError(file) {
+      this.file = [];
+      this.uploadFile = [];
+      this.$Message.success("不支持文件上传格式");
+    },
+    handleMaxSize(file) {
+      this.file = [];
+      this.uploadFile = [];
+      this.$Message.success("文件大小超出限制范围");
+    },
+    delectFile(keyID) { // 未使用方法
+      // 删除总展示文件里的当前文件
+      this.file = this.file.filter(item => {
+        return item.name != name;
+      });
+      // 删除需要上传文件数据里的当前文件
+      this.uploadFile = this.uploadFile.filter(item => {
+        return item.KeyID != keyID;
+      });
+    },
+    upload() {
+      this.file = [];
+      for (let i = 0; i < this.uploadFile.length; i++) {
+        console.log(this.uploadFile);
+        
+        let exName = this.uploadFile[i].name.split(".");
+        let newName =
+          this.userID + " " + this.username + "." + exName[exName.length - 1];
+        this.uploadFile[i]["newName"] = newName;
+        this.$refs.upload.post(this.uploadFile[i]);
+      }
+    },
+    reset() {
+      this.userID = "";
+      this.username = "";
+      this.file = [];
+      this.uploadFile = [];
+    },
+    uploadSuccess(res, file, fileList) {
+      //上传成功后删除待上传文件
+      setTimeout(() => {
+        this.reset();
+        this.loadingStatus = false;
+        this.$Message.success("上传成功");
+      }, 1500);
+    },
+    uploadError(err, file, fileList) {
+      setTimeout(() => {
+        this.reset();
+        this.loadingStatus = false;
+        this.$Message.warning("上传失败");
+      }, 1500);
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.information * {
+  margin: 20px 0 0;
+}
+.submit * {
+  width: 15%;
+  margin: 20px 10px 0;
+}
+.uploadFiled {
+  margin: 10px auto 0;
+  width: 50%;
+}
+.uploadList {
+  margin-top: 10px;
+  font-size: 20px;
+}
 p {
-  font-size: 36px
-}
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+  font-size: 30px;
+  font: #004465;
 }
 </style>
